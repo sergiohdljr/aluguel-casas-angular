6c4041aa578ac3b40f5e56a429bb7fae20758849
sergiohdljr <sergio.tjf1@gmail.com>
2023-07-03 16:30:42 -0300

    lesson 7

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,7 +15,9 @@ import { Housinglocation } from '../housinglocation';
       </form>
     </section>
     <section class="results">
-      <app-housing-location></app-housing-location>
+      <app-housing-location
+        [housingLocation]="housingLocation"
+      ></app-housing-location>
     </section>
   `,
   styleUrls: ['./home.component.css'],
@@ -26,7 +28,8 @@ export class HomeComponent {
     name: 'Test Home',
     city: 'Test city',
     state: 'ST',
-    photo: 'assets/example-house.jpg',
+    photo:
+      'https://static01.nyt.com/images/2019/06/02/realestate/31EXCLUSIVE-SOPRANOS-slide-4SEN/31EXCLUSIVE-SOPRANOS-slide-4SEN-superJumbo.jpg',
     avaliableUnits: 99,
     wifi: true,
     laundry: false,
